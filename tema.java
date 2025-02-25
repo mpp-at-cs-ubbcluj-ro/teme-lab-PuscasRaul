0d307c9a32bac80f5a76f9746fa60d1caf83ac22
Raul <puscas.raul.pr@gmail.com>
2025-02-25 18:28:52 +0200

    tema

--- tema.java
+++ tema.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-	System.out.println("AAAAAAAAAAAAAAAAAAAAAAA");
+public class tema {
+	public static void main(String[] args) {
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAA");
+	}
 }
